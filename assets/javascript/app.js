6ad2e36b821287fa39725dba465448695d02fed3
Judy Hu <judy.hu9@gmail.com>
2018-11-01 15:51:10 -0400

    check

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,20 @@ $.ajax({
 }).then(function(response) {
   console.log(response);
 });
+
+/////////////////////////
+var api = "https://api.giphy.com/v1/gifs/search?";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var query = "&q=cats";
+
+function setup() {
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+}
+
+function gotData(giphy) {
+  for (var i = 0; i < giphy.data.length; i++) {
+    createImg(giphy.data[i].images.original.url);
+  }
+}

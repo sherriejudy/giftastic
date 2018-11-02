293b35474d4e763c38fe09de49c01632fcca8849
Judy Hu <judy.hu9@gmail.com>
2018-11-01 22:47:54 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,102 @@
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+let api_key = "mohzNO5atvsiXS7XdAVpUevJaoSJtEdc";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
+$(document).ready(function() {
+  //Global Variables
+  var gifs = [];
+  var searchTerms = ["banana", "cereal", "steak"];
 
-/////////////////////////
-var api = "https://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=dc6zaTOxFJmzC";
-var query = "&q=cats";
+  // START!!
+  start();
 
-function setup() {
-  noCanvas();
-  var url = api + apiKey + query;
-  loadJSON(url, gotData);
-}
+  // Set up
+  $("#add").on("click", function() {
+    let text = $("#input").val();
+    addButton(text);
+  });
 
-function gotData(giphy) {
-  for (var i = 0; i < giphy.data.length; i++) {
-    createImg(giphy.data[i].images.original.url);
+  // Functions
+  function start() {
+    // Add all buttons to screen
+    for (var i = 0; i < searchTerms.length; i++) {
+      addButton(searchTerms[i]);
+    }
   }
-}
+
+  function addButton(text) {
+    var btn = $("<button>");
+    btn.addClass("btn");
+    btn.addClass("btn-info");
+    // Adds margin of 1 on right
+    btn.addClass("mr-1");
+    btn.text(text);
+    btn.on("click", function() {
+      clearGifs();
+      pullAndPlaceGifs($(this).text());
+    });
+    $("#buttons").append(btn);
+  }
+
+  function clearGifs() {
+    $("#gifs").empty();
+  }
+
+  function pullAndPlaceGifs(searchTerm) {
+    let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${searchTerm}&limit=10&offset=0&rating=PG13&lang=en`;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      let data = response.data;
+      processGifs(data);
+    });
+  }
+
+  function processGifs(gifList) {
+    for (var i = 0; i < gifList.length; i++) {
+      var gifItem = gifList[i];
+      var processedGif = {
+        rating: gifItem.rating,
+        animatedURL: gifItem.images.fixed_width.url,
+        stillURL: gifItem.images.fixed_width_still.url
+      };
+      placeGif(processedGif);
+    }
+  }
+
+  function placeGif(gif) {
+    var card = $("<div>");
+    var cardImage = $("<img>");
+    var cardBody = $("<div>");
+    var cardText = $("<p>");
+    var smallText = $("<small>");
+
+    card.addClass("card");
+    card.attr("animating", "false");
+    cardImage.addClass("card-img-top");
+    cardImage.attr("src", gif.stillURL);
+    cardBody.addClass("card-body");
+    cardText.addClass("card-text");
+    smallText.addClass("text-muted");
+    smallText.text("Rating: " + gif.rating);
+
+    cardText.append(smallText);
+    cardBody.append(cardText);
+    card.append(cardImage);
+    card.append(cardBody);
+
+    card.on("click", function() {
+      if ($(this).attr("animating") === "false") {
+        $(this).attr("animating", "true");
+        $(this)
+          .find("img")
+          .attr("src", gif.animatedURL);
+      } else {
+        $(this).attr("animating", "false");
+        $(this)
+          .find("img")
+          .attr("src", gif.stillURL);
+      }
+    });
+    $("#gifs").append(card);
+  }
+});
